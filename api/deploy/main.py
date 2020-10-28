925d53a2bdf51b30b86682870392868e612d1dcd
Nicolò Valenza <nico.vale.spazio@outlook.com>
2020-10-28 11:46:17 +0100

    Aggiunta di uno script fastapi

---
+++ api/deploy/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import os
+import json
+
+app = FastAPI()
+
+
+
+class Render_template:
+    def __init__(self, templatename):
+        self.template = templatename
+    def renderhtml(self):
+        with open(f"templates/{self.template}", "r") as index:
+            return str(index.read())
+    def renderjson(self):
+        with open(f"templates/{self.template}", "r") as index:
+            return json.dumps(str(index.read()))
+
+@app.get("/", response_class=HTMLResponse)
+async def root():
+    index = Render_template("index.html")
+    return index.renderhtml()
+
+@app.get("/api")
+async def root():    
+    return {"Hello World!": "ciao"}
